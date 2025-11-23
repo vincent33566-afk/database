1d6e54f8dad3a2031509d247515acb785cb3e41e
Arthur <vincent33566@gmail.com>
2025-11-23 11:32:01 +0800

    Fix formatting of token.js

--- token.js
+++ token.js
@@ -1,6 +1,6 @@
 {
   "tokens" : [
     "8494784916:AAGPoFXDt76CXCqtfIHbeCFeOEUr4NLqMiY",
-    " 8451625664:AAHD0yUmHUQDuMvojnhF0K_MgUnOoKlogjk"
+    "8451625664:AAHD0yUmHUQDuMvojnhF0K_MgUnOoKlogjk"
   ]
 }
