fdb7b313e0213f52d403dbb3ac8326ba0323cc4c
Arthur <vincent33566@gmail.com>
2025-12-10 22:56:58 +0800

    Add new tokens to token.js

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens" : [
     "8494784916:AAGPoFXDt76CXCqtfIHbeCFeOEUr4NLqMiY",
-    "8451625664:AAHD0yUmHUQDuMvojnhF0K_MgUnOoKlogjk"
+    "8451625664:AAHD0yUmHUQDuMvojnhF0K_MgUnOoKlogjk",
+    " 8572349018:AAH1GfsojEaA7NuGIHmPw-5N7Us1eyYidso"
   ]
 }
